232b2e6e4862c23f6bbb66a55d7a57ee15561d51
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2014-11-07 13:24:42 +0100

    Fixing project deployment target and compile error

--- src/NetworkClock.m
+++ src/NetworkClock.m
@@ -6,6 +6,7 @@
   ╚══════════════════════════════════════════════════════════════════════════════════════════════════╝*/
 
 #import <netinet/in.h>
+#import <arpa/inet.h>
 #import "NetworkClock.h"
 
 @interface NetworkClock (PrivateMethods)
@@ -178,8 +179,6 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"assoc-good" object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"assoc-fail" object:nil];
 }
-
-#import <arpa/inet.h>
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
   ┃ ... obtain IP address, "xx.xx.xx.xx", from the sockaddr structure ...                            ┃
